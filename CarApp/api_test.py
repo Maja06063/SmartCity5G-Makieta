9f1e286e9dd478bdf671881d8c2df3baa48f8807
Maja06063 <259707@student.pwr.edu.pl>
2023-04-30 21:20:26 +0200

    Przygotowanie serwera

--- CarApp/api_test.py
+++ CarApp/api_test.py
@@ -3,11 +3,17 @@ from requests import get, post
 firstInt = 1
 seconInt = 2
 
-#parameters = {"distance_cm": firstInt, "token": seconInt}
-parameters = {"user": firstInt, "pass": seconInt}
+# Odkomentuj właściwy test:
 
-#response = get("http://127.0.0.1:5000/direction", json = parameters)
+# 1. Test POSTa z danymi logowania:
+parameters = {"user": firstInt, "pass": seconInt}
 response = post("http://127.0.0.1:5000/auth", json = parameters)
+###
+
+# 2. Test GETa z odległością:
+#parameters = {"distance_cm": firstInt, "token": seconInt}
+#response = get("http://127.0.0.1:5000/direction", json = parameters)
+###
 
 if response.status_code == 200:
     print(response.json())
--- SerwerApp/main.py
+++ SerwerApp/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import json
 
+# Enum kierunków ruchu:
 class WhereDrive():
 
     GO_LEFT = 1
@@ -8,30 +9,47 @@ class WhereDrive():
     GO_STRAIGHT = 3
     DESTINATION = 4
 
+"""
+Funkcja check_authentication służy do sprawdzenia czy samochód przesłał
+poprawny login i hasło i jeśli tak, do wygenerowania i zachowania na potem tokenu.
+"""
+def check_authentication(user: str, passw: int) -> str:
+
+    #Tu ma być kod, który wylicza token (Ola)
+    return "123" # placeholder
+
+"""
+Funkcja which_direction służy do sprawdzenia w jakiej odległości jest samochód (o podanym tokenie),
+do wyliczenia kierunku w jakim ten samochód ma jechać oraz zwrócenia go.
+"""
+def which_direction(token: str, distance_cm: int) -> WhereDrive:
+
+    # Tu ma być kod do zamiany dystansu na kierunek (Ola)
+    return WhereDrive.GO_STRAIGHT
+
 app = Flask('serwer')
-
-@app.route('/direction', methods=['GET'])
-def direction():
-    
-    data = json.loads(request.data)
-    if data["token"] and data["distance_cm"]:
-        
-       #Tu ma być funkcja do zamiany dystansu na kierunek (Ola)
-        turn_direction = WhereDrive.GO_STRAIGHT # ona ma zwracac turn_direction
-
-            #zmienna w jsonie: w pythonie
-    return {"turn_direction": turn_direction}
 
 @app.route('/auth', methods=['POST'])
 def authentication():
-    
+
     data = json.loads(request.data)
     if data["user"] and data["pass"]:
-        
-       #Tu ma być funkcja, która wylicza token (Ola)
-        token = 0 # ona ma  token
 
-            #zmienna w jsonie: w pythonie
+
+        token = check_authentication(data["user"], data["pass"])
+
+    # Zmienna w jsonie: zmienna w pythonie
     return {"token": token}
+
+@app.route('/direction', methods=['GET'])
+def direction():
+
+    data = json.loads(request.data)
+    if data["token"] and data["distance_cm"]:
+
+        turn_direction = which_direction(data["token"], data["distance_cm"])
+
+    # Zmienna w jsonie: zmienna w pythonie
+    return {"turn_direction": turn_direction}
 
 app.run()
