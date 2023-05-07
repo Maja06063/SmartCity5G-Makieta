1af97de5541381004fc160b4347a031a4b15e1fc
Nielsfish <117576038+Nielsfish@users.noreply.github.com>
2023-05-07 13:23:25 +0200

    Nie wiem
    Na razie to co mi sie udalo, ale totalnie nie wiem i wolalam maina nie psuc

--- CarApp/api_test.py
+++ CarApp/api_test.py
@@ -1,7 +1,16 @@
 from requests import get, post
+from hashlib import md5
 
 firstInt = 1
-seconInt = 2
+seconInt = '2'
+
+# Haszowanie hasła
+
+passoword_bytes = seconInt.encode() # encode zwraca bajty ze stringa password
+md5_bites = md5(passoword_bytes).digest() # md5 wylicza md5 (hasz) z podanych bajtow, a digest zamienia obiekt md5 na bajty
+seconInt = int.from_bytes(md5_bites, 'big') # zamiana bajtow na inta, big oznacza ze najbardziej znaczacy bajt jest na początku
+# metoda from_bytes która konwertuje bajty na inty
+# print("Haszowane haslo: ", seconInt)
 
 # Odkomentuj właściwy test:
 
--- SerwerApp/main.py
+++ SerwerApp/main.py
@@ -16,7 +16,23 @@ poprawny login i hasło i jeśli tak, do wygenerowania i zachowania na potem tok
 def check_authentication(user: str, passw: int) -> str:
 
     #Tu ma być kod, który wylicza token (Ola)
-    return "123" # placeholder
+
+    # Na "twardo" sprawdzanie danych logowania do testowania makiety
+    if user == 1 and passw == 266003691477286198901011725417809479212:
+        # print("Poprawne logowanie")
+        token = {"Authorization": "Bearer TokenForExperiments"} # Tworzenie tokenu ?
+
+    return token # zwracanie utworzonego tokenu
+
+# Obszar globalny na zmienne globalne do funkcji poniżej
+#going_straight = 1
+#before_turn_left = 2
+#after_turn_left = 3
+#before_turn_right = 4
+#after_turn_right = 5
+#on_destination = 6
+my_state = 'prosto'    # stan w jakim jest samochód (na początku jedzie prosto)
+iter_turn = 0
 
 """
 Funkcja which_direction służy do sprawdzenia w jakiej odległości jest samochód (o podanym tokenie),
@@ -25,6 +41,44 @@ do wyliczenia kierunku w jakim ten samochód ma jechać oraz zwrócenia go.
 def which_direction(token: str, distance_cm: int) -> WhereDrive:
 
     # Tu ma być kod do zamiany dystansu na kierunek (Ola)
+    # zapamietanie w jakies zmiennej globalnej ostatni stan (duzo ifow mnie czeka i ify w ifach )
+    # stany: jedzie prosto, skret (przed i po), docelowe miejsce 
+    #my_state = 1 # stan w jakim jest samochód (na początku jedzie prosto)
+    d_max = 10  # odleglosc od sciany, przy ktorym skret, moze sie zmienic - potrzebne wymiary fizyczne makiety
+    d_destination = 40 # wstepnie założę, że miejsce parkingowe będzie 40 cm od ściany
+
+    if my_state == 'prosto':
+        if distance_cm <= d_max:
+            my_state = 'przed skretem'
+        if distance_cm > d_max:
+            return WhereDrive.GO_STRAIGHT
+        
+    if iter_turn == 0 or iter_turn == 1: # Jak trasa na szkicach to skrety w lewo sa dwa pierwszy i drugi skret
+        if my_state == 'przed skretem':
+            my_state = 'po skrecie'
+            iter_turn = iter_turn + 1
+            return WhereDrive.GO_LEFT
+    
+    if iter_turn == 2 and distance_cm <= d_destination: # Jak trasa na szkicach to skret w prawo jest na miejsce parkingowe
+        if my_state == 'przed skretem':
+            my_state = 'na miejscu'
+            iter_turn = iter_turn + 1
+            return WhereDrive.GO_RIGHT
+
+    if my_state == 'po skrecie':
+        if distance_cm > d_max:
+            my_state = 'prosto'
+            return WhereDrive.GO_STRAIGHT
+    if my_state == 'na miejscu':
+        return WhereDrive.DESTINATION
+
+    # if distance_cm > d_max:
+        # return WhereDrive.GO_STRAIGHT
+    # if distance_cm <= d_max:
+        # skad wiadomo czy ma skrecic w lewo czy w prawo ? tez kinda na twardo (ja wybieram konkretne miejsce i trase)
+        # return WhereDrive.GO_LEFT
+    # jaki if na wiedzenie czy jest na miejscu ? 
+    
     return WhereDrive.GO_STRAIGHT
 
 app = Flask('serwer')
